92f2911598083c4fa96d2eb7f1b3d1084a1452a7
Jchips <20979096+Jchips@users.noreply.github.com>
2023-12-20 19:50:11 -0800

    another github actions test

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -8,7 +8,7 @@ const request = supertest(app);
 // Turn database on for test
 beforeAll(async () => {
   await sequelize.sync();
-  console.log('sequelize in server.test', sequelize); // delete later
+  // console.log('sequelize in server.test', sequelize); // delete later
 });
 
 // Turn database off for test
