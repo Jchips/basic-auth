14f0c848d774df1fc80f645fe3feebad195c8a36
Jchips <20979096+Jchips@users.noreply.github.com>
2023-12-22 02:48:41 -0800

    github actions table testt

--- src/auth/models/userModel.js
+++ src/auth/models/userModel.js
@@ -5,7 +5,6 @@ const bcrypt = require('bcrypt');
 const { Sequelize, DataTypes } = require('sequelize');
 
 const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite:memory' : process.env.DATABASE_URL;
-// const DATABASE_URL = 'sqlite:memory';
 
 // db instance
 const sequelize = new Sequelize(DATABASE_URL);
