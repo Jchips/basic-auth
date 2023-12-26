149308b44e5bfa117759a18f1fc6b29a1ad83d45
Jchips <20979096+Jchips@users.noreply.github.com>
2023-12-25 16:29:44 -0800

    testing github actions with new code

--- __tests__/signin.test.js
+++ __tests__/signin.test.js
@@ -1,39 +1,50 @@
 'use strict';
 
-const signin = require('../src/auth/signin');
 const base64 = require('base-64');
+const middleware = require('../src/auth/signin');
 const { sequelize, Users } = require('../src/auth/models/userModel');
-// const Users = require('../src/auth/models/userModel');
 
-let user = { username: 'dog', password: 'password123' };
+let userInfo = {
+  admin: { username: 'admin-basic', password: 'password' },
+};
 
-// Turn database on for test
+// Pre-load our database with fake users
 beforeAll(async () => {
-  await sequelize.sync({force: true});
-  await console.log('signin synced');
-  return await Users.create(user);
+  await sequelize.sync();
+  await Users.create(userInfo.admin);
 });
-
-// Turn database off for test
 afterAll(async () => {
-  console.log('signin dropped');
-  return await sequelize.drop();
+  await sequelize.drop();
 });
 
-describe('signin middleware', () => {
+describe('Auth Middleware', () => {
+
+  // admin:password: YWRtaW46cGFzc3dvcmQ=
+  // admin:foo: YWRtaW46Zm9v
+
+  // Mock the express req/res/next that we need for each middleware call
   const req = {};
-  const res = {};
+  const res = {
+    status: jest.fn(() => res),
+    send: jest.fn(() => res),
+  };
   const next = jest.fn();
 
-  it('user signs in as expected', () => {
-    const basicAuthString = base64.encode(`${user.username}:${user.password}`);
-    req.headers = {
-      authorization: `Basic ${basicAuthString}`,
-    };
+  describe('user authentication', () => {
 
-    return signin(req, res, next)
-      .then(() => {
-        expect(next).toHaveBeenCalledWith();
-      });
+    it('logs in an admin user with the right credentials', () => {
+      let basicAuthString = base64.encode(`${userInfo.admin.username}:${userInfo.admin.password}`);
+
+      // Change the request to match this test case
+      req.headers = {
+        authorization: `Basic ${basicAuthString}`,
+      };
+
+      return middleware(req, res, next)
+        .then(() => {
+          expect(next).toHaveBeenCalledWith();
+        });
+
+    });
   });
 });
